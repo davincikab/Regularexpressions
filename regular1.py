f843c63c15f262c320e91e4845f1b18a45503936
davincikab <davidnganganjer079@gmail.com>
2020-02-14 10:07:31 +0300

    Using re.findall to get all groups

--- regular1.py
+++ regular1.py
@@ -16,5 +16,10 @@ print(match_2.group(0))
 
 # Using search to specify begining of a string
 pattern_3 = r"^Kenya"
+string_3 = "Kenya ni yetu"
 match_3 =  re.search(pattern_3,string_2)
-print(match_3.group())
+print(match_3)
+
+#Using findall
+match_4 = re.findall(pattern_2,string_2)
+print(match_4)
