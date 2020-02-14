ff1460e12912ea3ec97e865834d44d8bca9538be
davincikab <davidnganganjer079@gmail.com>
2020-02-14 10:22:55 +0300

    Working with precompiled patterns

--- regular1.py
+++ regular1.py
@@ -14,12 +14,19 @@ string_2 = "Welcome to Kenya. It is Kenya"
 match_2 = re.search(pattern_2,string_2)
 print(match_2.group(0))
 
-# Using search to specify begining of a string
+# Using search to specify begining of a string: retunr the first matched instance
 pattern_3 = r"^Kenya"
 string_3 = "Kenya ni yetu"
 match_3 =  re.search(pattern_3,string_2)
 print(match_3)
 
-#Using findall
+#Using findall: returns an list of match
 match_4 = re.findall(pattern_2,string_2)
 print(match_4)
+
+# Working with pre compiled patterns
+string_5 = "Welcome to 1942"
+ppatern = re.compile(r"(.*\d+)")
+matches = ppatern.search(string_5)
+print(matches.group(0))
+print(matches.group(1))
